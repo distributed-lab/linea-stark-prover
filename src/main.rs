6b154ca93ecdc14d527ecafcbf826a8472f93efa
nazarevsky <nazarevsky.ua@gmail.com>
2025-01-07 14:43:30 +0200

    Add trace parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod air;
-use corset::cgo;
+use corset::{cgo, import};
 
 fn main() {
-    let corset = cgo::corset_from_file("zkevm.bin").unwrap();
+    let mut corset = cgo::corset_from_file("zkevm.bin").unwrap();
+    import::parse_binary_trace("traces/trace1.lt", &mut corset, true).unwrap();
+
+    corset.columns.registers.iter().for_each(|r| {
+        if let Some(backing) = r.backing() {
+            println!("{:?}", backing);
+        } else {
+            println!("None");
+        }
+    })
 }
