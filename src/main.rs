2fc8fb3d01aa43ffdd46be34ee386b16da46ccca
olegfomenko <oleg.fomenko2002@gmail.com>
2025-01-10 18:22:43 +0200

    parsing traces

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod air;
+use corset::cgo::Trace;
 use corset::{cgo, import};
 
 fn main() {
     let mut corset = cgo::corset_from_file("zkevm.bin").unwrap();
-    import::parse_binary_trace("traces/trace1.lt", &mut corset, true).unwrap();
+    import::parse_binary_trace(
+        "traces/4181195-4181272.conflated.v0.8.0-rc3.lt",
+        &mut corset,
+        true,
+    )
+    .unwrap();
 
-    corset.columns.registers.iter().for_each(|r| {
-        if let Some(backing) = r.backing() {
-            println!("{:?}", backing);
-        } else {
-            println!("None");
-        }
-    })
+    let trace = Trace::from_constraints(&corset);
+    println!("{:?}", trace.ids)
 }
